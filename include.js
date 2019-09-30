9353842785cd075dc5e4c125c0b6182a444879e7
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2019-10-01 00:02:26 +0530

    Added ellipse loading animation

--- include.js
+++ include.js
@@ -1,14 +1,20 @@
 let cards = [
-    //  Add your card in this section
-    {
-      artName: "Triangle",
-      pageLink: "./Art/Joy/triangle.html",
-      imageLink: "./Art/Joy/triangle.gif",
-      author: "Joy",
-      githubLink: "https://github.com/royranger"
-    }
+  //  Add your card in this section
+  {
+    artName: "Triangle",
+    pageLink: "./Art/Joy/triangle.html",
+    imageLink: "./Art/Joy/triangle.gif",
+    author: "Joy",
+    githubLink: "https://github.com/royranger"
+  },
+  {
+    artName: "Ellipse Loader",
+    pageLink: "./Art/VaibhavKhulbe/EllipseLoader.html",
+    imageLink: "./Art/VaibhavKhulbe/ellipseLoader.gif",
+    author: "Vaibhav Khulbe",
+    githubLink: "https://github.com/Kvaibhav01"
+  }
 ];
-
 
 // +--------------------------------------------------------------------------------+
 // +                                                                                +
@@ -20,23 +26,26 @@ let cards = [
 // You don't need to modify this
 let contents = [];
 Shuffle(cards).forEach(c => {
-    contents.push([
-        `<li class="card">` +
-          `<a href='${c.pageLink}'>` +
-            `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-          `</a>` +
-          `<div class="flex-content">` +
-            `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-            `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-          `</div>` +
-        `</li>`
-    ])
+  contents.push([
+    `<li class="card">` +
+      `<a href='${c.pageLink}'>` +
+      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+      `</a>` +
+      `<div class="flex-content">` +
+      `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
+      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+      `</div>` +
+      `</li>`
+  ]);
 });
-
 
 document.getElementById("cards").innerHTML = contents;
 
 function Shuffle(o) {
-    for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-};
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
+}
