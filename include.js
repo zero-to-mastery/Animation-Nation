eed4988b0e26cf66f607abd368a802031242b4f2
Godnon <godnondsilva@gmail.com>
2019-10-01 21:12:35 +0530

    include.js artName changed

--- include.js
+++ include.js
@@ -141,7 +141,7 @@ let cards = [
     githubLink: "https://github.com/OctavianIlies"
   },
   {
-    artName: "ZTM Color Animation",
+    artName: "ZTM Color",
     pageLink: "./Art/Godnon/index.html",
     imageLink: "./Art/Godnon/ZTManim.gif",
     author: "Godnon",
