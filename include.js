0fbb1e8be63e56a9ec17f4578b011810e54737bb
Terence Biney <tereflech17@gmail.com>
2019-10-15 14:14:10 -0400

    hackoctober fest letter animation

--- include.js
+++ include.js
@@ -887,6 +887,13 @@ let cards = [
     imageLink: "https://media.giphy.com/media/RKSRPGiIsy1f3Ji3j1/giphy.gif",
     author: "Juan T.",
     githubLink: "https://github.com/jmtellez"
+    },
+    {
+        artName: "Letters flipUp",
+        pageLink: "./Art/TerenceBiney/index.html",
+        imageLink: "./Art/TerenceBiney/lettersanimate.gif",
+        author: "Terence Biney"
+        githubLink: "https://github.com/Tereflech17"
     }
 ];
 
