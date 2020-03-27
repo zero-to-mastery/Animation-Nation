be2d06bbba7a1d588c9e299261a1faec01fb53fa
Ulisses <ulisses1024073@gmail.com>
2020-03-27 14:08:03 -0300

    adding my artsyness to the project

--- include.js
+++ include.js
@@ -1866,6 +1866,13 @@ let cards = [
     imageLink: "./Art/samya/samya.gif",
     author: "Samya Thakur",
     githubLink: "https://github.com/samyathakur"
+  },
+  {
+    artName: "Egg_Loading",
+    pageLink: "./Art/egg_loading/index.html",
+    imageLink: "./Art/samya/egg_loading.gif",
+    author: "Ulisse Dantas",
+    githubLink: "https://github.com/ulissesnew"
   }
 ];
 
