fddff98eeab7d3e9fb3c190b7dab647d33b95478
Naoki Kojima <dark.light9640@gmail.com>
2020-06-09 13:14:45 +0900

    adding my artsyness to the project

--- include.js
+++ include.js
@@ -3597,6 +3597,13 @@
     imageLink: "./Art/sanmitra/index.gif",
     author: "sanmitra",
     githubLink: "https://github.com/sanmitra1999"
+  },
+  {
+    artName: "Ball-clear",
+    pageLink: "./Art/Naok000/index.html",
+    imageLink: "./Art/Naok000/ball-clear.gif",
+    author: "Naok000",
+    githubLink: "https://github.com/Naok000"
   }
 
 ]
