ebbe61d4b52897c9fcb0856a641cce014dce91bf
Hannah <Hannah@HFK.cable.rcn.com>
2020-06-30 18:02:55 -0400

    adding cat in space?

--- include.js
+++ include.js
@@ -3598,16 +3598,16 @@
     author: "sanmitra",
     githubLink: "https://github.com/sanmitra1999"
   },
-    {
-      artName: "Hannahmation Nation",
-      pageLink: "./Art/Hannahmation-Nation/index.html",
-      imageLink: "./Art/Hannahmation-Nation/cat-in-space.gif",
-      authot: "Hannah",
-      githubLink: "https://github.com/hannahfk"
-    }
+  {
+    artName: "Hannahmation Nation",
+    pageLink: "./Art/Hannahmation-Nation/index.html",
+    imageLink: "./Art/Hannahmation-Nation/cat-in-space.png",
+    author: "Hannah",
+    githubLink: "https://github.com/hannahfk"
+  }
       
 
-]
+];
 
 // +--------------------------------------------------------------------------------+
 // +                                                                                +
