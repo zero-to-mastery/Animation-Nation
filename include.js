20547375bb20ccc7a2cb6e9b728783245a8f2d5f
gaurav gupta <gg7827091@gmail.com>
2020-07-28 18:36:14 +0530

    adding my artsyness to the project

--- include.js
+++ include.js
@@ -4502,6 +4502,13 @@
     author: "Kaitlyn Friden",
     githubLink: "https://github.com/kfriden",
   },
+  {
+    artname: "Coloured Boxes",
+    pageLink: "./Art/gavlon/index.html",
+    imageLink: "./Art/gavlon/colouredboxes.gif",
+    author: "Gaurav",
+    githubLink: "https://github.com/gaurav241998",
+  },
 ];
 
 // +--------------------------------------------------------------------------------+
