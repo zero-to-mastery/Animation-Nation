570c2208f83f2aab7cd3b07a546d2b5e25efad77
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-22 12:44:18 +0000

    (CI) format code

--- Art/luiavag/script.js
+++ Art/luiavag/script.js
@@ -1,18 +1,18 @@
 function hasClassName(inElement, inClassName) {
-  var regExp = new RegExp("(?:^|\\s+)" + inClassName + "(?:\\s+|$)");
+  var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
   return regExp.test(inElement.className);
 }
 
 function addClassName(inElement, inClassName) {
   if (!hasClassName(inElement, inClassName))
-    inElement.className = [inElement.className, inClassName].join(" ");
+    inElement.className = [inElement.className, inClassName].join(' ');
 }
 
 function removeClassName(inElement, inClassName) {
   if (hasClassName(inElement, inClassName)) {
-    var regExp = new RegExp("(?:^|\\s+)" + inClassName + "(?:\\s+|$)", "g");
+    var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
     var curClasses = inElement.className;
-    inElement.className = curClasses.replace(regExp, " ");
+    inElement.className = curClasses.replace(regExp, ' ');
   }
 }
 
@@ -23,25 +23,25 @@ function toggleClassName(inElement, inClassName) {
 }
 
 function toggleShape() {
-  var shape = document.getElementById("shape");
-  if (hasClassName(shape, "ring")) {
-    removeClassName(shape, "ring");
-    addClassName(shape, "cube");
+  var shape = document.getElementById('shape');
+  if (hasClassName(shape, 'ring')) {
+    removeClassName(shape, 'ring');
+    addClassName(shape, 'cube');
   } else {
-    removeClassName(shape, "cube");
-    addClassName(shape, "ring");
+    removeClassName(shape, 'cube');
+    addClassName(shape, 'ring');
   }
 
   // Move the ring back in Z so it's not so in-your-face.
-  var stage = document.getElementById("stage");
-  if (hasClassName(shape, "ring"))
-    stage.style.webkitTransform = "translateZ(-200px)";
-  else stage.style.webkitTransform = "";
+  var stage = document.getElementById('stage');
+  if (hasClassName(shape, 'ring'))
+    stage.style.webkitTransform = 'translateZ(-200px)';
+  else stage.style.webkitTransform = '';
 }
 
 function toggleBackfaces() {
-  var backfacesVisible = document.getElementById("backfaces").checked;
-  var shape = document.getElementById("shape");
-  if (backfacesVisible) addClassName(shape, "backfaces");
-  else removeClassName(shape, "backfaces");
+  var backfacesVisible = document.getElementById('backfaces').checked;
+  var shape = document.getElementById('shape');
+  if (backfacesVisible) addClassName(shape, 'backfaces');
+  else removeClassName(shape, 'backfaces');
 }
--- Art/vraoblr/script.js
+++ Art/vraoblr/script.js
@@ -1,15 +1,15 @@
 $(window).load(function () {
-  var body = $("body"),
-    universe = $("#universe"),
-    solarsys = $("#solar-system");
+  var body = $('body'),
+    universe = $('#universe'),
+    solarsys = $('#solar-system');
 
   var init = function () {
     body
-      .removeClass("view-2D opening")
-      .addClass("view-3D")
+      .removeClass('view-2D opening')
+      .addClass('view-3D')
       .delay(2000)
       .queue(function () {
-        $(this).removeClass("hide-UI").addClass("set-speed");
+        $(this).removeClass('hide-UI').addClass('set-speed');
         $(this).dequeue();
       });
   };
@@ -18,38 +18,38 @@ $(window).load(function () {
     universe.removeClass().addClass(view);
   };
 
-  $("#toggle-data").click(function (e) {
-    body.toggleClass("data-open data-close");
+  $('#toggle-data').click(function (e) {
+    body.toggleClass('data-open data-close');
     e.preventDefault();
   });
 
-  $("#toggle-controls").click(function (e) {
-    body.toggleClass("controls-open controls-close");
+  $('#toggle-controls').click(function (e) {
+    body.toggleClass('controls-open controls-close');
     e.preventDefault();
   });
 
-  $("#data a").click(function (e) {
-    var ref = $(this).attr("class");
+  $('#data a').click(function (e) {
+    var ref = $(this).attr('class');
     solarsys.removeClass().addClass(ref);
-    $(this).parent().find("a").removeClass("active");
-    $(this).addClass("active");
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
     e.preventDefault();
   });
 
-  $(".set-view").click(function () {
-    body.toggleClass("view-3D view-2D");
+  $('.set-view').click(function () {
+    body.toggleClass('view-3D view-2D');
   });
-  $(".set-zoom").click(function () {
-    body.toggleClass("zoom-large zoom-close");
+  $('.set-zoom').click(function () {
+    body.toggleClass('zoom-large zoom-close');
   });
-  $(".set-speed").click(function () {
-    setView("scale-stretched set-speed");
+  $('.set-speed').click(function () {
+    setView('scale-stretched set-speed');
   });
-  $(".set-size").click(function () {
-    setView("scale-s set-size");
+  $('.set-size').click(function () {
+    setView('scale-s set-size');
   });
-  $(".set-distance").click(function () {
-    setView("scale-d set-distance");
+  $('.set-distance').click(function () {
+    setView('scale-d set-distance');
   });
 
   init();
