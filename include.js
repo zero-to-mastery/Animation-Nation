033399a5c653625c3e862049fca8048c7212356a
Simron Adiwal <simron@users-MacBook-Air.local>
2020-10-28 23:13:33 -0700

    adding Sim-animation directory and updated include.js file

--- include.js
+++ include.js
@@ -4263,6 +4263,13 @@ let cards = [
     imageLink: "./Art/Lidor's Animation/animation.gif",
     author: 'LidorAsher',
     githubLink: 'https://github.com/lidorasher11'
+  },
+  {
+    artName: "Basketball God"
+    pageLink: './Art/Sim-animation/index.html',
+    imageLink: './Art/Sim-animation/project-screenshot.png',
+    author: 'Sim',
+    githubLink: 'https://github.com/sim-a-19'
   }
 ];
 
