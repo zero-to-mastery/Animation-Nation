39f49fc55fc65dee964a9d4624c6d11ecf7c1718
Ubibimbap <72666983+Ubibimbap@users.noreply.github.com>
2020-10-29 15:07:02 +0100

    adding a psychedelic animation

--- include.js
+++ include.js
@@ -4263,6 +4263,13 @@ let cards = [
     imageLink: "./Art/Lidor's Animation/animation.gif",
     author: 'LidorAsher',
     githubLink: 'https://github.com/lidorasher11'
+    {
+      artName: "Otherside",
+      pageLink: "./Art/Otherside/ubi.html",
+      imageLink: "./Art/Otherside/recording.gif",
+      author: 'Ubibimbap',
+      githubLink: 'https://github.com/Ubibimbap'
+    }
   }
 ];
 
