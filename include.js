6255d26788a312c6c1a642e8517527478b4e1e75
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-11-11 20:58:30 +0000

    (CI) format code

--- include.js
+++ include.js
@@ -4274,15 +4274,13 @@ let cards = [
 
   {
     artName: "Ziyao's Animation",
-    pageLink: "./Art/robot/robot_index.html",
-    imageLink: "./Art/robot/robot.gif",
+    pageLink: './Art/robot/robot_index.html',
+    imageLink: './Art/robot/robot.gif',
     author: 'Ziyao',
     githubLink: 'https://github.com/ziyaoc3'
   }
 ];
 
-
-  
 // +--------------------------------------------------------------------------------+
 // +                                                                                +
 // +                  YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS                 +
