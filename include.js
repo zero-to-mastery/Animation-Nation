1e00193a4dff25474f48b7e37e519227aea2e151
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-11-11 21:10:07 +0000

    (CI) format code

--- include.js
+++ include.js
@@ -4272,9 +4272,9 @@ let cards = [
     githubLink: 'https://github.com/lidorasher11'
   },
   {
-    artName: "Bouncing Balls",
-    pageLink: "./Art/EyeOfAthena/index.html",
-    imageLink: "./Art/EyeOfAthena/cover.png",
+    artName: 'Bouncing Balls',
+    pageLink: './Art/EyeOfAthena/index.html',
+    imageLink: './Art/EyeOfAthena/cover.png',
     author: 'EyeOfAthena',
     githubLink: 'https://github.com/EyeOfAthena/bouncing-ball'
   },
