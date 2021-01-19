4e2642a067629db1e07d95c577351a851209f145
iriswdq0504 <76138192+iriswdq0504@users.noreply.github.com>
2021-01-19 09:41:46 -0600

    adding my artness to the project

--- include.js
+++ include.js
@@ -4592,6 +4592,17 @@ let cards = [
     author: 'ianhawe',
     githubLink: 'https://github.com/ianhawe'
   },
+ {
+    artName: 'iris',
+    pageLink: './Art/iris/index.html',
+    imageLink: './Art/iris/trance.gif',    
+    author: 'iriswdq0504',
+    githubLink: 'https://github.com/iriswdq0504'
+  },
+
+
+
+
   {
     artName: 'Fun with balls!',
     pageLink: './Art/miguelDalberto/funWithBalls/index.html',
