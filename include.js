ce83785ad8e90e779f95be263c028ab7e646d548
Alida Diphoorn <67009403+adiphoorn@users.noreply.github.com>
2021-01-22 20:10:46 +0100

    added my name artsyness to the project (#1044)
    * added my name artsyness to the project
    
    * changed pagelink
    
    * changed pagelink again
    
    * changed h1

--- include.js
+++ include.js
@@ -4815,6 +4815,13 @@ let cards = [
     author: 'Liza',
     githubLink: 'https://github.com/ElizavetaZhukova1'
   }
+  ,
+  {
+    pageLink: './Art/Aliedje/index.html',
+    imgeLink: './Art/Aliedje/test.png',
+    author: 'Alida',
+    githubLink: 'https://github.com/adiphoorn'
+  }
 ];
 // +--------------------------------------------------------------------------------+
 // +                                                                                +
