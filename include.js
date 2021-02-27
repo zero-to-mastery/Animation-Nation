dc9b574ad7297a9cdfef542dade935df12091d57
Võ Thành Luân <67989630+luancauthu2907@users.noreply.github.com>
2021-02-27 22:26:32 +0700

    adding my project (#1097)

--- include.js
+++ include.js
@@ -4995,7 +4995,14 @@ let cards = [
     imageLink: './Art/circle-animation/animate.gif',
     author: 'Vimal',
     githubLink: 'https://github.com/vimalraveendra'
-  }
+  },
+    {
+      artName: 'VN Luan Animation',
+      pageLink: './Art/LuanAnimation/index.html',
+      imageLink: './Art/LuanAnimation/animation.gif',
+      author: 'Luan2907',
+      githubLink: 'https://github.com/luancauthu2907'
+    }
 ];
 
 // +--------------------------------------------------------------------------------+
