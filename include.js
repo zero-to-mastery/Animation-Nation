8e501ee66b8b9e29d01751066308e11e45d02b07
roshanamjad <65924851+roshanamjad@users.noreply.github.com>
2021-03-27 21:07:14 +0500

    adding again (#1125)
    * adding again
    
    * Name Change

--- include.js
+++ include.js
@@ -5056,7 +5056,14 @@ let cards = [
     imageLink: './Art/Animated-Bike-Wheels/bike.gif',
     author: 'Joey Kyber',
     githubLink: 'https://github.com/jtkyber'
-  }
+  },
+  {
+    artName: 'My-Anim.R',
+    pageLink: './Art/My-Anim.R/index.html',
+    imageLink: './Art/My-Anim.R/BackGround.gif',
+    author: 'Roshan Amjad',
+    githubLink: 'https://github.com/roshanamjad'
+  },
 ];
 
 // +--------------------------------------------------------------------------------+
