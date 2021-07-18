2d7357f680ff63f449f72449a7c4809d533f7c06
Erik Avetisyan <erik.avetisyan1@gmail.com>
2021-07-18 15:12:14 +0200

    adding my flags

--- include.js
+++ include.js
@@ -5448,6 +5448,13 @@ let cards = [
     imageLink: './Art/Piano/piano.gif',
     author: 'Ali Akhavan',
     githubLink: 'https://github.com/aliakhavanrad'
+  },
+  {
+    artName: 'Switching flags',
+    pageLink: './Art/ErikAvet/index.html',
+    imageLink: './Art/ErikAvet/flag.gif',
+    author: 'Erik Avetisyan',
+    githubLink: 'https://github.com/ErikAvetisyan'
   }
 ];
 // +--------------------------------------------------------------------------------+
