e39382ac3e12e6303c7de042a93112380bdc820f
zabarullah <60830173+zabarullah@users.noreply.github.com>
2021-09-15 13:47:49 +0100

    Adding my art to the project

--- include.js
+++ include.js
@@ -5469,7 +5469,15 @@ let cards = [
     imageLink: './Art/black-hole-vortex/black-hole-vortex.gif',
     author: 'Bo-wei Chen',
     githubLink: 'https://github.com/Rayologist'
+  },
+  {
+    artName: 'Basic Text Animation',
+    pageLink: './Art/zabarullah/index.html',
+    imageLink: './Art/zabarullah/screenshot.png',
+    author: 'Zabar Ullah',
+    githubLink: 'https://github.com/zabarullah'
   }
+
 ];
 // +--------------------------------------------------------------------------------+
 // +                                                                                +
