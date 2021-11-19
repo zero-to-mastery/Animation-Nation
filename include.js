b5bc9d3812be6d075530cd090a9fde06d4c990f9
Krubel Sahle Habteyes <habteyes83@gmail.com>
2021-11-19 05:17:11 -0500

    adding my animation

--- include.js
+++ include.js
@@ -5561,6 +5561,11 @@ let cards = [
     imageLink: './Art/complementaryColors/complementaryColors.gif',
     author: 'Hilary',
     githubLink: 'https://github.com/hwilson2563'
+  },
+  {
+    pageLink: './Art/Rubberband-Animation/index.html/',
+    author: 'Krubel',
+    githubLink: 'https://github.com/Krubel-Habteyes'
   }
 ];
 
