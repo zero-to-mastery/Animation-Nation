24a44222fbd5ca91c3a6b336eba4d699189b66de
satoshimasterbilly <billy.mcdavid2@yahoo.com>
2022-02-15 14:27:07 +0000

    add my rainbow art to the project

--- include.js
+++ include.js
@@ -1,11 +1,11 @@
 let cards = [
- {
-	artName: 'Goa - Beach',
+  {
+    artName: 'Goa - Beach',
     pageLink: './Art/Shailesh/index.html',
     imageLink: './Art/Shailesh/Goa - Beach.jpg',
     author: 'Shailesh',
     githubLink: 'https://github.com/shaileshkr7'
-  }
+  },
   {
     artName: 'Gloomy Loading Ring',
     pageLink: './Art/kevinsana/index.html',
@@ -5596,11 +5596,18 @@ let cards = [
     githubLink: 'https://github.com/joesayat'
   },
   {
-    artName: 'Animatron',
-    pageLink: './Art/animatron/index.html',
-    imageLink: './Art/animatron/trance.gif',
-    author: 'Billy',
-    githubLink: 'https://github.com/satoshimasterbilly/Animation-Nation'
+    artName: 'Funky Alien',
+    pageLink: './Art/Funky Alien/index.html',
+    iamgeLink: './Art/Funky Alien/Domcake-Dancing-Alien.gif',
+    author: 'Jenique Knoesen',
+    githubLink: 'https://github.com/jenique22'
+  },
+  {
+  artName: 'Rainbowtron'
+    pageLink: './Art/rainbowtron/index.html',
+    imageLink: './Art/rainbowtron/trance.jpeg',
+    author: 'Satoshimasterbilly',
+    githubLink: 'https://github.com/satoshimasterbilly'
   }
 ];
 
@@ -5637,22 +5644,3 @@ function Shuffle(o) {
   );
   return o;
 }
-
-let cards = [
-  //  Add your card in this section
-  {
-    pageLink: './Art/chrisg/index.html',
-    imageLink: './Art/chrisg/redwhiteblue.gif',
-    author: 'ChrisG',
-    githubLink: 'https://github.com/chrisgithubok'
-  }
-];
-let cards = [
-  {
-    artName: 'Funky Alien',
-    pageLink: './Art/Funky Alien/index.html',
-    iamgeLink: './Art/Funky Alien/Domcake-Dancing-Alien.gif',
-    author: 'Jenique Knoesen',
-    githubLink: 'https://github.com/jenique22'
-  }
-];
