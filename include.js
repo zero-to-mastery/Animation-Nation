fc61a0f3643c949d162a91130a8723a84071897d
GabrielAvramescu <98615377+GabrielAvramescu@users.noreply.github.com>
2022-02-18 20:19:41 +0100

    adding my Art to the project (#1450)

--- include.js
+++ include.js
@@ -5608,6 +5608,12 @@ let cards = [
     iamgeLink: './Art/Funky Alien/Domcake-Dancing-Alien.gif',
     author: 'Jenique Knoesen',
     githubLink: 'https://github.com/jenique22'
+  },
+  {
+    pageLink: './Art/GabrielArt/GabrielArt.html',
+    imageLink: './Art/GabrielArt/ZTM.jpg',
+    author: 'GabrielAvramescu',
+    githubLink: 'https://github.com/GabrielAvramescu'
   }
 ];
 
@@ -5623,14 +5629,14 @@ let contents = [];
 Shuffle(cards).forEach((c) => {
   contents.push([
     `<li class="card">` +
-      `<a href='${c.pageLink}'>` +
-      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-      `</a>` +
-      `<div class="flex-content">` +
-      `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-      `</div>` +
-      `</li>`
+    `<a href='${c.pageLink}'>` +
+    `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+    `</a>` +
+    `<div class="flex-content">` +
+    `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
+    `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+    `</div>` +
+    `</li>`
   ]);
 });
 
