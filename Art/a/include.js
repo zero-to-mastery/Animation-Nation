1f21d5269230767a0733d90a3c0a4ee856d0a8a2
IoanaPopovici <ioana_gabrielaa19@yahoo.com>
2022-06-02 10:43:11 +0300

    am adaugat

---
+++ Art/a/include.js
@@ -0,0 +1,31 @@
+let cards = [
+    //  Add your card in this section
+    {
+        artName: 'Your art name here',
+        pageLink: './Art/Joy/a/index.html',
+        imageLink: './Art/Joy/a/style.gif',
+        author: 'Ioana',
+        githubLink: 'https://github.com/IoanaPopovici'
+    },
+    {
+        artName: 'My art',
+        pageLink: './Art/Joy/a/index.html',
+        imageLink: './Art/Joy/a/style.gif',
+        author: 'Ioana',
+        githubLink: 'https://github.com/IoanaPopovici'
+    },
+    {
+        artName: 'Art',
+        pageLink: './Art/Joy/a/index.html',
+        imageLink: './Art/Joy/a/style.gif',
+        author: 'Ioana',
+        githubLink: 'https://github.com/IoanaPopovici'
+    },
+    {
+        artName: 'Seious',
+        pageLink: './Art/Joy/a/index.html',
+        imageLink: './Art/Joy/a/style.gif',
+        author: 'Ioana',
+        githubLink: 'https://github.com/IoanaPopovici'
+    }
+];
