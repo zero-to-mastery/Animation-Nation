4a1862b4b60d4ed339a6815a8fa1b196b788bf3a
WibawaArif <arifwb.work@gmail.com>
2022-08-05 11:20:21 +0700

    adding my quoters to the project

--- include.js
+++ include.js
@@ -6078,6 +6078,13 @@ let cards = [
     imageLink: './Art/can-you-feel-my-heartbeat/heartbeat.png',
     author: 'Vanessa Vun',
     githubLink: 'https://github.com/vanessavun'
+  },
+  {
+    artName: 'Quoters',
+    pageLink: './Art/Quoters/index.html',
+    imageLink: './Art/Quoters/giphy.gif',
+    author: 'Arif Wibawa',
+    githubLink: 'https://github.com/wibawaarif'
   }
 ];
 
