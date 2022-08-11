d413baa7fe18960f97305bc29ac55aaf21b9ae56
julien-muke <julien.muke@gmail.com>
2022-08-11 08:39:33 +0200

    adding my art to the project

--- include.js
+++ include.js
@@ -6078,7 +6078,15 @@ let cards = [
     imageLink: './Art/can-you-feel-my-heartbeat/heartbeat.png',
     author: 'Vanessa Vun',
     githubLink: 'https://github.com/vanessavun'
+  },
+  {
+    artName: 'Color Palette with Pure CSS Animation',
+    pageLink: './Art/color-palette-with-pure-css-animation/dist/index.html',
+    imageLink: './Art/color-palette-with-pure-css-animation/animation.gif',
+    author: 'julien muke',
+    githubLink: 'https://github.com/julien-muke'
   }
+
 ];
 
 // +--------------------------------------------------------------------------------+
