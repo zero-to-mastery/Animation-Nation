0c9c18d53f06926c13d458f6b025b5a48b44443e
Nicolas Le Flohic <leflohic.nicolas@gmail.com>
2022-09-02 00:41:13 +0200

    Add my animation

--- include.js
+++ include.js
@@ -6239,20 +6239,27 @@ let cards = [
     imageLink: './Art/erikanimation/corazon.gif',
     author: 'Erika',
     githubLink: 'https://github.com/erikaaquino'
-},
-{
+  },
+  {
     artName: 'Colorful Heart',
     pageLink: './Art/cr-animation/index.html',
     imageLink: './Art/cr-animation/index.html/colorful-heart.png',
     author: 'Cristina',
     githubLink: 'https://github.com/crsecu'
-},
-{
+  },
+  {
     artName: 'wink',
     pageLink: './Art/Robrigado/index.html',
     imageLink: './Art/Robrigado/wink.gif',
     author: 'Robrigado',
     githubLink: 'https://github.com/Robrigado'
+  },
+  {
+    artName: 'Text Spawn',
+    pageLink: './Art/NlefTextAnimation/index.html',
+    imageLink: './Art/NlefTextAnimation/text_anim.gif',
+    author: 'Nicolef',
+    githubLink: 'https://github.com/NLeflohic'
   }
 ];
 
@@ -6268,14 +6275,14 @@ let contents = [];
 Shuffle(cards).forEach((c) => {
   contents.push([
     `<li class="card">` +
-      `<a href='${c.pageLink}'>` +
-      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-      `</a>` +
-      `<div class="flex-content">` +
-      `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-      `</div>` +
-      `</li>`
+    `<a href='${c.pageLink}'>` +
+    `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+    `</a>` +
+    `<div class="flex-content">` +
+    `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
+    `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+    `</div>` +
+    `</li>`
   ]);
 });
 
