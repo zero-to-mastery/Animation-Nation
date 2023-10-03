7add3498e39fbcafe3326c4fc45b9a4ef3e2b988
Sanusi Abdulkadir <94573138+sanusisusi@users.noreply.github.com>
2023-10-03 12:42:11 +0100

    Update include.js

--- include.js
+++ include.js
@@ -13,6 +13,13 @@ let cards = [
     author: 'Ibaad',
     githubLink: 'https://github.com/ibaaddurrani'
   }
+   {
+    artName: 'SquBounce',
+    pageLink: './Art/Sanusi/index.html',
+    imageLink: './Art/Sanusi/sanusi-animation.gif',
+    author: 'Sanusi',
+    githubLink: 'https://github.com/sanusisusi'
+  }
 ];
 
 // +--------------------------------------------------------------------------------+
