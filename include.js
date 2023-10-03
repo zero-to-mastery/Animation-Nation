ec792dc0072af83f4b258be409649ad0f2b95d72
Shubhojyoti Das <shubhojyotidas800@gmail.com>
2023-10-03 21:51:55 +0530

    push my contributions

--- include.js
+++ include.js
@@ -4,6 +4,7 @@ let cards = [
     pageLink: './Art/ghost-2362003/triangle_run.html',
     imageLink: './Art/ghost-2362003/triangle_run.png',
     author: 'Shubhojyoti Das',
+    
     githubLink: 'https://github.com/ghost-2362003'
   }
 ];
