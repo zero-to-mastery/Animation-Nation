55eb10e4c4adaa933d93fc1e212a663a90e52151
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-10-04 01:28:57 -0400

    name updated

--- include.js
+++ include.js
@@ -479,7 +479,7 @@ let cards = [
     artName: 'Moving Car',
     pageLink: './Art/Ravkeerat02/car.html',
     imageLink: './Art/Ravkeerat02/car.gif',
-    author: 'Joy',
+    author: 'Ravkeerat Singh',
     githubLink: 'https://github.com/Ravkeerat02'
   },
   {
