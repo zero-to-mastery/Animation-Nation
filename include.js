7eb5c65cb19b6f58647b67c2075334d9f1dd1cc6
Shubhamm33 <mohite.shubhu61@gmail.com>
2023-10-06 00:05:43 +0530

    stroboscopic gif

--- include.js
+++ include.js
@@ -1765,6 +1765,13 @@ let cards = [
     author: 'Divyansh',
     githubLink: 'https://github.com/okaydivyansh' 
   },
+  {
+    artName: 'Stroboscopic effect',
+    pageLink: './Art/Shubhamm33/index.html',
+    imageLink: './Art/Shubhamm33/stroboscopic.gif',
+    author: 'Shubhamm33',
+    githubLink: 'https://github.com/Shubhamm33'
+  }
 ];
 
 
