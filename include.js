b52452621d5fdadd0ed46248faf70984d1b6e54d
Aman <dv451197@gmail.com>
2023-10-06 18:51:14 +0530

    circles added

--- include.js
+++ include.js
@@ -85,6 +85,13 @@ let cards = [
     githubLink: 'https://github.com/rishi17003'
   },
   {
+    artName: 'Circles',
+    pageLink: './Art/AmanRawat/index.html',
+    imageLink: './Art/AmanRawat/circles.gif',
+    author: 'Aman Rawat',
+    githubLink: 'https://github.com/amanr-dev'
+  },
+  {
     artName: 'hanisntsolo',
     pageLink: './Art/hanisntsolo/hanisntsolo.html',
     imageLink: './Art/hanisntsolo/hanisntsolo.gif',
