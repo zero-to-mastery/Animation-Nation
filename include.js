cfcbba7c30e644ebf55d6f88c34e1c7c36e9e3f5
Dan Chui <dchui108@gmail.com>
2023-10-06 17:58:29 +0900

    add dan-chui art files

--- include.js
+++ include.js
@@ -1534,6 +1534,13 @@ let cards = [
     imageLink: './Art/haitruong-tech/typewriter-animation.gif',
     author: 'haitruong-tech',
     githubLink: 'https://github.com/haitruong-tech'
+  },
+  {
+    artName: 'Ramen Bowl',
+    pageLink: './Art/dan-chui/index.html',
+    imageLink: './Art/dan-chui/ramen_bowl.gif',
+    author: 'dan-chui',
+    githubLink: 'https://github.com/dan-chui'
   }
 ];
 
