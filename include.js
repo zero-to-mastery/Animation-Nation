d06c6427a1dcdebd0ede8db4a38c3c7fdec01b77
Rishabh <rishabh22204901@gmail.com>
2023-10-12 10:33:25 +0530

    Update include.js
    add

--- include.js
+++ include.js
@@ -6,6 +6,7 @@ let cards = [
     author: 'Sandesh-Pyakurel',
     githubLink: 'https://github.com/Sandesh-Pyakurel'
   },
+  
   {
     artName: 'cube-with-Letter-Animation',
     pageLink: './Art/sufyanhabib/index.html',
