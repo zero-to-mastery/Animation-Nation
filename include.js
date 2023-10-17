ab7d22487a5a07fd61dbf8abc374c3037bdff18b
pleomorph <16924710+pleomorph@users.noreply.github.com>
2023-10-17 11:50:34 -0400

    pleomorph's Hacktoberfest contribution (#2178)
    Co-authored-by: Laureline Paris <32878345+LaurelineP@users.noreply.github.com>

--- include.js
+++ include.js
@@ -1891,7 +1891,14 @@ let cards = [
     imageLink: './Art/JohnRTitor/Sample.gif',
     author: 'JohnRTitor',
     githubLink: 'https://github.com/JohnRTitor'
-  }
+  },
+  {
+    artName: 'Happy Hacktoberfest 2023',
+    pageLink: './Art/pleomorph/index.html',
+    imageLink: './Art/pleomorph/preview-pleomorph.gif',
+    author: 'pleomorph',
+    githubLink: 'https://github.com/pleomorph'
+  },
 ];
 
 // +--------------------------------------------------------------------------------+
@@ -1906,14 +1913,14 @@ let contents = [];
 Shuffle(cards).forEach((c) => {
   contents.push([
     `<li class="card">` +
-      `<a href='${c.pageLink}'>` +
-      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-      `</a>` +
-      `<div class="flex-content">` +
-      `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-      `</div>` +
-      `</li>`
+    `<a href='${c.pageLink}'>` +
+    `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+    `</a>` +
+    `<div class="flex-content">` +
+    `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
+    `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+    `</div>` +
+    `</li>`
   ]);
 });
 
