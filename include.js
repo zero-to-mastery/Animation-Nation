8b388582b3502d5faea19510d97208bbb9b9bfee
Ruchir Gavshinde <132195333+ruchircodes24@users.noreply.github.com>
2023-10-21 21:51:04 +0530

    Adding my animation project to the list (#2250)
    * Adding my animation project to the list
    
    * final update
    
    ---------
    
    Co-authored-by: Laureline Paris <laurelinedev@gmail.com>

--- include.js
+++ include.js
@@ -2100,6 +2100,13 @@ let cards = [
     githubLink: 'https://github.com/gabriel-conde'
   },
   {
+    artName: 'Score a Goal',
+    pageLink: './Art/ruchircodes24/index.html',
+    imageLink: './Art/ruchircodes24/Score.gif',
+    author: 'Ruchir',
+    githubLink: 'https://github.com/ruchircodes24'
+  },
+  {
     artName: 'lightsaber glowing animation',
     pageLink: './Art/mohamedabomosallam/index.html', 
     imageLink: './Art/mohamedabomosallam/lightsaber.gif', 
