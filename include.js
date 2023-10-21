4324b4cb238f6ac2102041fbba0a70bd109a4a64
Ahmed Raza <41878215+ahmedrazabaloch@users.noreply.github.com>
2023-10-22 03:37:51 +0500

    Update include.js
    added my name with the img reference

--- include.js
+++ include.js
@@ -1,4 +1,11 @@
 let cards = [
+   {
+    artName 'Loading Dot Animation',
+    pageLink '.Artahmedrazaindex.html',
+    imageLink '.ArtahmedrazaLoading-Dot.gif',
+    author 'Ahmed raza',
+    githubLink 'httpsgithub.comahmedrazabaloch'
+  },
   {
     artName: 'Robot Image Animation',
     pageLink: './Art/aniketmdinde/index.html',
