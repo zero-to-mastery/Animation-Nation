d71fb4e87585ebf664bc46bec4073d0f54929c90
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-10-22 11:43:08 +0200

    Revert "Add New Loader (#2267)"
    This reverts commit 871da7cabfd1439c17bd754d8cc870b337fd3a68.

--- include.js
+++ include.js
@@ -2133,13 +2133,6 @@ let cards = [
     githubLink: 'https://github.com/gabriel-conde'
   },
   {
-    artName: 'Rolling Square Loader',
-    pageLink: './Art/psykat1611/index.html',
-    imageLink: './Art/psykat1611/loader.gif',
-    author: 'psykat1116',
-    githubLink: 'https://github.com/psykat1116'
-  },
-  {
     artName: 'Score a Goal',
     pageLink: './Art/ruchircodes24/index.html',
     imageLink: './Art/ruchircodes24/Score.gif',
