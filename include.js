8af24638327f5083e24117a89626ba8a4ac273d0
shashank <shashankmpatil3535@gmail.com>
2023-10-25 00:22:17 +0530

    Loading

--- include.js
+++ include.js
@@ -2292,6 +2292,13 @@ let cards = [
     githubLink: 'https://github.com/Shubhamkashyap1601'
   },
   {
+    artName: 'Loading animation',
+    pageLink: './Art/ShashankPatil35/index.html',
+    imageLink: './Art/ShashankPatil35/LoadingGIF.gif',
+    author: 'ShashankPatil',
+    githubLink: 'hhttps://github.com/ShashankPatil35'
+  },
+  {
     artName: 'Mouse Eyes Animation',
     pageLink: './Art/sandeep-dss/index.html',
     imageLink: 'Art/sandeep-dss/mouse.gif',
