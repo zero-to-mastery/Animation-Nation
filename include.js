b7f947986c5610a78879695cfdf01a831fc44d64
cpasigna90 <cpasigna90@gmail.com>
2023-10-30 09:36:03 +0800

    adding my art to the project

--- include.js
+++ include.js
@@ -2669,6 +2669,13 @@ let cards = [
     author: 'Jeeva Ramanathan',
     githubLink: 'https://github.com/JeevaRamanathan'
   },
+  {
+    artName: 'Box Color Changing', // change this to the name of your artwork
+    pageLink: './Art/cpasigna90/boxcolorchange.html', // change this
+    imageLink: './Art/cpasigna90/animationColorBox.gif', // change this
+    author: 'Christian', // use your name
+    githubLink: 'https://github.com/cpasigna90' // change this
+  }
 ];
 
 /* -------------------------------------------------------------------------- */
