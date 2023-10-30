44392a543b98832369ea49997689a04000758ed2
Suparna <89165591+Suparnaen@users.noreply.github.com>
2023-10-30 11:26:19 -0700

    initial commit for submission (#2374)
    * initial commit for submission
    
    initial commit for submission
    
    * Feedback comments fixed
    
    1.Submission folder renamed to my github repo name.
    2. Contribution details added in include.js.
    Please review
    
    * Update include.js
    
    updated include.js to resolve merge conflict
    
    ---------
    
    Co-authored-by: Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>

--- include.js
+++ include.js
@@ -41,6 +41,7 @@ let cards = [
     author: 'Krish Gautam',
     githubLink: 'https://github.com/Kris248'
   },
+  
   {
     artName: 'Ball Animation',
     pageLink: './Art/daemonvk18/animation..html',
@@ -2600,6 +2601,12 @@ let cards = [
     githubLink: 'https://github.com/rudrakushwaha'
   },
   {
+    artName: 'Smiling Pumpkin Animation',
+    pageLink: './Art/Suparnaen/smiling_pumpkin.html',
+    imageLink: 'Art/Suparnaen/smiling-pumpkin-animation.gif',
+    author: 'Suparna Das',
+    githubLink: 'https://github.com/Suparnaen'
+  },
     artName: 'Bouncing Balls with colour change',
     pageLink: './Art/Vivek-GuptaXCode/bouncing-ball.html',
     imageLink: './Art/Vivek-GuptaXCode/bouncing-ball.gif',
