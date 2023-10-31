c7da44b1ea711ddf961d46c94a4b313b69ed069a
stormworm9 <149419878+stormworm9@users.noreply.github.com>
2023-10-31 22:35:38 +0530

    Added a  Cube Animation (#2409)
    * Create CubeAnimation.html
    
    * "Uploaded a css file and gif"
    
    * Update include.js
    
    * fix syntax error in include.js
    
    ---------
    
    Co-authored-by: Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>

--- include.js
+++ include.js
@@ -1,5 +1,12 @@
 let cards = [
-  {
+   {
+    artName: 'CubeAnimation',
+    pageLink: './Art/stormworm9/CubeAnimation/CubeAnimation.html',
+    imageLink: './Art/stormworm9/CubeAnimation/CubeAnimation.gif',
+    author: 'stormworm9',
+    githubLink: 'https://github.com/stormworm9
+   },
+   {
     artName: 'RingAnimation',
     pageLink: './Art/stormworm9/RingAnimation/ring.html',
     imageLink: './Art/stormworm9/RingAnimation/ring.gif',
