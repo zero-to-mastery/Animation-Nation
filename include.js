9537a8702e9428c19c412c9df0d70250ecb1247e
stormworm9 <149419878+stormworm9@users.noreply.github.com>
2023-11-01 00:00:56 +0530

    Update include.js

--- include.js
+++ include.js
@@ -1,11 +1,11 @@
 let cards = [
-  {
+    {
     artName: 'GoldenCoin',
     pageLink: './Art/stormworm9/GoldenCoin/GoldenCoin.html',
     imageLink: './Art/stormworm9/GoldenCoin/GoldenCoin.gif',
     author: 'stormworm9',
     githubLink: 'https://github.com/stormworm9'
-  }
+  },
   {
     artName: 'RingAnimation',
     pageLink: './Art/stormworm9/RingAnimation/ring.html',
