1bae85aa2dbe8d4c70f22feb0e0e14735a3ec183
stormworm9 <149419878+stormworm9@users.noreply.github.com>
2023-11-01 19:34:58 +0530

    Added Triangle Projection Animation (#2406)
    * Create ring.html
    
    "HTML File For Ring Animation"
    
    * Create styles.css
    
    "CSS File For Ring Animation"
    
    * Add Gif file
    
    * Update include.js
    
    * Create projection.html
    
    * Add files via upload
    
    * Gif file
    
    * Update include.js
    
    * Update include.js
    
    * Update include.js
    
    * Update include.js
    
    * Update include.js
    
    * Update include.js
    
    Co-authored-by: Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
    
    * Delete Art/stormworm9/TriangleProjection/TriangleProjection.gif
    
    * Added new gif
    
    ---------
    
    Co-authored-by: Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>

--- include.js
+++ include.js
@@ -33,7 +33,17 @@ let cards = [
     imageLink: './Art/stormworm9/RingAnimation/ring.gif',
     author: 'stormworm9',
     githubLink: 'https://github.com/stormworm9'
-  },
+   },
+
+  {
+    artName: 'TriangleProjection',
+    pageLink: './Art/stormworm9/TriangleProjection/projection.html',
+    imageLink: './Art/stormworm9/TriangleProjection/TriangleProjection.gif',
+    author: 'stormworm9',
+    githubLink: 'https://github.com/stormworm9'
+  }, 
+
+
   {
     artName: 'Loading Page',
     pageLink: './Art/SaumyaKumar-09/Animation%204/index.html',
@@ -41,6 +51,7 @@ let cards = [
     author: 'SaumyaKumar-09',
     githubLink: 'https://github.com/SaumyaKumar-09'
   },
+
   {
     artName: 'Windows start animation',
     pageLink: './Art/SaumyaKumar-09/Animation%202/index.html',
