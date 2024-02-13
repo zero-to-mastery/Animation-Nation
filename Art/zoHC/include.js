13f33a501e1c5dc4cd4357f2113196337b0550d9
0x01001010 <154769989+0x01001010@users.noreply.github.com>
2024-02-12 17:11:12 -0800

    Add GitHub Octocat artwork and animation

---
+++ Art/zoHC/include.js
@@ -0,0 +1,11 @@
+let cards = [
+  // Add your card in this section
+  {
+    artName: 'GitHub Octocat',
+    pageLink: './Art/zoHC/index.html',
+    imageLink: './Art/zoHC/octocat-art.gif',
+    author: 'zoHC',
+    githubLink: 'https://github.com/zoHC'
+  },
+  // ... other cards, maybe?
+];
