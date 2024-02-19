bc14af6968b46c3a5c5aef57cacbcaceb2a6ff27
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-18 22:34:04 -0500

    adding artsyness to the project

--- include.js
+++ include.js
@@ -2889,20 +2889,27 @@ let cards = [
     author: 'Shazeen Thowfeek',
     githubLink: 'https://github.com/shazeen-1990'
   },
-  {    
+  {
     artName: 'Moving Clouds Animation',
     pageLink: './Art/artursniegowski/index.html',
     imageLink: './Art/artursniegowski/moving_clouds.gif',
     author: 'Artur Sniegowski',
     githubLink: 'https://github.com/artursniegowski'
   },
-  { 
+  {
     artName: 'First Animation',
     pageLink: './Art/Kalliran/index.html',
     imageLink: './Art/Kalliran/Animation-Nation.gif',
     author: 'Kalliran',
     githubLink: 'https://github.com/Kalliran'
   },
+  {
+    artName: 'Circle',
+    pageLink: './Art/vitali/index.html',
+    imageLink: './Art/vitali/gif.png',
+    author: 'ChatGPT',
+    githubLink: 'https://github.com/vitalipri'
+  }
 ];
 
 /* -------------------------------------------------------------------------- */
