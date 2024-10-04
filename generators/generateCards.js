20c39c7aa63cdd7d8c172aa9e72833dc2409eba7
Laureline <laurelinedev@gmail.com>
2024-10-04 17:00:55 +0200

    [ fix/maintainance__hyphens-edge-cases ] - if more than one hyphen in dir. name: will get the real   - authorname *(based on found github url using curl)*   - projectName: being the remaining string

--- generators/generateCards.js
+++ generators/generateCards.js
@@ -1,43 +1,61 @@
 const fs = require("fs");
 const path = require("path");
+const { handleExtraHyphens} = require('./generateCards.services');
+
 
 const artDir = "./Art";
 
 // Function to generate the includes.js content
-function generateIncludes() {
+async function generateIncludes() {
   const studentDirs = fs
     .readdirSync(artDir)
     .filter((dir) => fs.lstatSync(path.join(artDir, dir)).isDirectory());
 
   const cards = [];
 
-  studentDirs.forEach((dir) => {
-    const projectPath = path.join(artDir, dir);
+	for(const dir of studentDirs){
+      const projectPath = path.join(artDir, dir);
+	  const dirDetailsStrings = dir.split('-');
+	  
+	  
+	  // handle name
+	  let authorName = dirDetailsStrings[0]; // Default authorName case - first word before first hyphen
+	  let projectName = dirDetailsStrings.slice(1).join('-'); // Default authorName case - Everything after the first hyphen
+	  
+	  /**
+	   * Two cases to consider
+	   * - username can have hyphen(s)
+	   * - title can have hyphen(s)
+	   * Overrides once more than one hyphens is detected by
+	   * checking existence of github user's URL
+	   */
+	  const BASIC_LENGTH_CASE = 2;
+	  const shouldHandleExtraHyphens = dirDetailsStrings.length > BASIC_LENGTH_CASE;
+	  if(shouldHandleExtraHyphens){
+	  	const data = await handleExtraHyphens(dirDetailsStrings);
+	  	authorName 	= data.authorName;
+	  	projectName = data.projectName;
+	  }
+  
+      projectName = projectName.replace(/([A-Z])/g, ' $1').trim(); // Handles camel case project name
 
-    // Use directory name, splitting on the last hyphen
-    const lastHyphenIndex = dir.lastIndexOf('-');
-    const authorName = dir.substring(0, lastHyphenIndex); // Everything before the last hyphen
-    let projectName = dir.substring(lastHyphenIndex + 1); // Everything after the last hyphen
-    projectName = projectName.replace(/([A-Z])/g, ' $1').trim(); // Handles camel case project name
-
-    // Handles underscores case in project name
-    if( projectName.includes('_') ){
-      projectName = projectName.replace(/(_)/g, ' ').trim();
-    }
-
-    const projectUrl = `./Art/${dir}/index.html`;
-    const projectImage = `./Art/${dir}/icon.png`;
-
-    // Add the project to the cards array
-    cards.push({
-      artName: projectName,
-      pageLink: projectUrl,
-      imageLink: projectImage,
-      author: authorName,
-      githubLink: `https://github.com/${authorName}`,
-      projectPath
-    });
-  });
+       // Handles underscores case in project name
+      if( projectName.includes('_') ){
+        projectName = projectName.replace(/(_)/g, ' ').trim();
+      }
+      const projectUrl = `./Art/${dir}/index.html`;
+      const projectImage = `./Art/${dir}/icon.png`;
+  
+      // Add the project to the cards array
+      cards.push({
+        artName: projectName,
+        pageLink: projectUrl,
+        imageLink: projectImage,
+        author: authorName,
+        githubLink: `https://github.com/${authorName}`,
+        projectPath
+      });
+	}
 
 
   // Write the content to includes.js file
@@ -45,3 +63,4 @@ function generateIncludes() {
 }
 
 generateIncludes();
+
---
+++ generators/generateCards.services.js
@@ -0,0 +1,53 @@
+const util = require("node:util");
+const { exec } = require('node:child_process');
+const execPromise = util.promisify(exec);
+
+
+/**
+ * Checks URL requests response status code
+ * - is 404: username does not exist
+ * - else: username does exist
+ */
+async function isExistingGithubUsername ( possibleUsername ){
+	const githubUrl = `https://github.com/${possibleUsername}`;
+	const curlRequestForStatusCode = `curl --head -s "${githubUrl}" | awk 'NR==1 {print $2}'`;
+
+	try {
+		const { stdout } = await execPromise(curlRequestForStatusCode);
+		return Number(stdout) !== 404;
+	} catch( error ){
+		return false;
+	}
+}
+
+/**
+ * Handles directory names with multiple hyphens to get 
+ * the real distinction between the official author name and
+ * the project name
+ * @returns authorName, projectName 
+ */
+async function handleExtraHyphens(detailsStrings){
+	let endUsernameIdx = detailsStrings.length - 1;
+	let dataDetails;
+
+	// while it is within author name possible range
+	while (endUsernameIdx >= -1 && !dataDetails ){
+		let potentialUsername = detailsStrings
+			.slice(0, endUsernameIdx)
+			.join('-');
+		const isExistingUserName = await isExistingGithubUsername(potentialUsername);
+
+		if ( isExistingUserName ){
+			dataDetails = {
+				authorName: potentialUsername,
+				projectName: detailsStrings.slice(endUsernameIdx).join('-')
+			}
+		}
+		endUsernameIdx -= 1;
+	}
+	return dataDetails;
+}
+
+module.exports = {
+	handleExtraHyphens
+}
