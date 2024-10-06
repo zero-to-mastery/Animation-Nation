6469f905df132ae425036306d662cb8e06e04bc6
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2024-10-07 02:47:17 +0300

    Updates: 1 Use of map Instead of forEach: Replaced forEach with map to directly create the cards array, making the code more functional and concise. 2 Combined String Replacements: Combined the camel case and underscore replacements into a single line for better readability. 3 Removed Redundant Comments: Removed comments that were self-explanatory based on the code.

--- generators/generateCards.js
+++ generators/generateCards.js
@@ -9,36 +9,29 @@ function generateIncludes() {
     .readdirSync(artDir)
     .filter((dir) => fs.lstatSync(path.join(artDir, dir)).isDirectory());
 
-  const cards = [];
-
-  studentDirs.forEach((dir) => {
+  const cards = studentDirs.map((dir) => {
     const projectPath = path.join(artDir, dir);
 
     // Use directory name, splitting on the last hyphen
     const lastHyphenIndex = dir.lastIndexOf('-');
     const authorName = dir.substring(0, lastHyphenIndex); // Everything before the last hyphen
     let projectName = dir.substring(lastHyphenIndex + 1); // Everything after the last hyphen
-    projectName = projectName.replace(/([A-Z])/g, ' $1').trim(); // Handles camel case project name
 
-    // Handles underscores case in project name
-    if( projectName.includes('_') ){
-      projectName = projectName.replace(/(_)/g, ' ').trim();
-    }
+    // Handle camel case and underscores in project name
+    projectName = projectName.replace(/([A-Z])/g, ' $1').replace(/_/g, ' ').trim();
 
     const projectUrl = `./Art/${dir}/index.html`;
     const projectImage = `./Art/${dir}/icon.png`;
 
-    // Add the project to the cards array
-    cards.push({
+    return {
       artName: projectName,
       pageLink: projectUrl,
       imageLink: projectImage,
       author: authorName,
       githubLink: `https://github.com/${authorName}`,
       projectPath
-    });
+    };
   });
-
 
   // Write the content to includes.js file
   fs.writeFileSync("public/cards.json", JSON.stringify(cards, null, 2));
