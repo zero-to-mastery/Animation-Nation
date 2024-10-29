f524656ee016a905ffdab21a099711788854da43
Laureline <laurelinedev@gmail.com>
2024-10-29 15:56:47 +0100

    Attempt fix - github action stuck on rmdirSync --> changed to rmSync = OK

--- generators/generateCards.js
+++ generators/generateCards.js
@@ -35,7 +35,7 @@ async function generateIncludes() {
 		});
 	} catch(error){
 		console.error(`[ ERROR x MISSING METADATA ]\n\t - Concerning folder: "${projectPath}"`);
-		fs.rmdirSync(projectPath)
+		fs.rmSync(projectPath, { recursive: true })
 		console.error(`\t|____ Folder removed: "${projectPath}"\n`);
 	}
    }
